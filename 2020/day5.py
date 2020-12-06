d7dc7fa0ca70c36cbd93fe66ff48aac3bd3ce614
Adrian Smith <adrian@17od.com>
2020-12-06 21:36:18 +0000

    Day 5, Part 2

--- 2020/day5.py
+++ 2020/day5.py
@@ -17,14 +17,27 @@ def load_seats(all_seat_chars):
     seats = []
     for seat_chars in all_seat_chars:
         row = num_from_binary_chars(seat_chars[:7], 'B')
-        col = num_from_binary_chars(seat_chars[7:], 'B')
+        col = num_from_binary_chars(seat_chars[7:10], 'R')
         seats.append((row, col))
     return seats
 
-def seats_ids(seats):
-    return [seat[0] * 8 + seat[1] for seat in seats]
+def seat_ids(seats):
+    return [(seat[0] * 8 + seat[1]) for seat in seats]
+
+def missing_seat_ids(seat_ids):
+    m = []
+    for row in range(0, 128):
+        for col in range(0, 8):
+            seat_id = row * 8 + col
+            if seat_id not in seat_ids:
+                m.append(seat_id)
+                print(f"Row: {row}, Col: {col}, Seat ID: {seat_id}")
+    return m
 
 if __name__ == '__main__':
     input_file = os.path.join(os.path.dirname(__file__), 'day5_input')
     seats = load_seats(read_input_raw(input_file))
-    print(max(seats_ids(seats)))
+    # print(max(seat_ids(seats)))
+
+    m = missing_seat_ids(seat_ids(seats))
+    # picked the odd looking missing seat in the output, 659
