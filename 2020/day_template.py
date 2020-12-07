28af5c270199128305660937273b5e8779cf8205
Adrian Smith <adrian@17od.com>
2020-12-07 21:09:09 +0000

    Added day template

---
+++ 2020/day_template.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from utils import read_input_raw
+import os
+
+def part1(data):
+    count = 0
+    return count
+
+def part2(data):
+    count = 0
+    return count
+
+if __name__ == '__main__':
+    input_file = os.path.join(os.path.dirname(__file__), 'day6_input')
+    data = read_input_raw(input_file)
+    print(part1(data))
+    # print(part2(data))
