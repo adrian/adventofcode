34d13890d578830a2b89cdd5187465c8138bda71
Adrian Smith <adrian@17od.com>
2020-12-07 21:08:16 +0000

    Day 6, part 2

--- 2020/day6.py
+++ 2020/day6.py
@@ -2,6 +2,7 @@
 
 from utils import read_input_raw
 import os
+import string
 
 def group_answers_generator(data):
     group_lines = []
@@ -27,7 +28,22 @@ def part1(data):
         count = count + num_unique_yeses(group_answers)
     return count
 
+def all_answered_yes(group_answers):
+    count = 0
+    for letter in string.ascii_lowercase:
+        answered_yes = [letter in individual_answers for individual_answers in group_answers]
+        if all(answered_yes):
+            count = count + 1
+    return count
+
+def part2(data):
+    count = 0
+    for group_answers in group_answers_generator(data):
+        count = count + all_answered_yes(group_answers)
+    return count
+
 if __name__ == '__main__':
     input_file = os.path.join(os.path.dirname(__file__), 'day6_input')
     data = read_input_raw(input_file)
-    print(part1(data))
+    # print(part1(data))
+    print(part2(data))
